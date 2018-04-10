7b70bccb7a354f1a5cb9690f2ce45e9cf9b4c561
F3llou <guillaume.beaumont@yahoo.fr>
2018-04-10 12:13:49 +0200

    Correction modele (manque encore Agent et Admin)

--- GestibankBackEnd/src/main/java/com/wha/springmvc/model/CompteRemun.java
+++ GestibankBackEnd/src/main/java/com/wha/springmvc/model/CompteRemun.java
@@ -1,5 +1,7 @@
 package com.wha.springmvc.model;
 
+import java.util.Date;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
@@ -12,24 +14,66 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("CR")
 public class CompteRemun extends Compte {
-	/*private float tauxInterets;*/
+	
+	private static double tauxInteret;
+	private Date dateRemun;
 	
 	public CompteRemun() {
 		super();
+		CompteRemun.setTauxInteret(3);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public CompteRemun(int id, double solde, String type) {
-		super(id, solde, type);
-
+	public CompteRemun(int id, double solde, Date dateDeb, Date dateRemun) {
+		super(id, solde, dateDeb);
+		CompteRemun.setTauxInteret(3);
+		this.dateRemun=dateRemun;
 	}
-	/*public float getTauxInterets() {
-        return tauxInterets;
-    }
-    
-    public double calculInterets() {
-        solde=solde+solde*tauxInterets;
-        return solde;}
-    
-}*/
+
+	public static double getTauxInteret() {
+		return tauxInteret;
+	}
+
+	public static void setTauxInteret(double tauxInteret) {
+		CompteRemun.tauxInteret = tauxInteret;
+	}
+
+	public Date getDateRemun() {
+		return dateRemun;
+	}
+
+	public void setDateRemun(Date dateRemun) {
+		this.dateRemun = dateRemun;
+	}
+
+	@Override
+	public String toString() {
+		return "CompteRemun [getDateRemun()=" + getDateRemun() + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((dateRemun == null) ? 0 : dateRemun.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompteRemun other = (CompteRemun) obj;
+		if (dateRemun == null) {
+			if (other.dateRemun != null)
+				return false;
+		} else if (!dateRemun.equals(other.dateRemun))
+			return false;
+		return true;
+	}
+	
 }
